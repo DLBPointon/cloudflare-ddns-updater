0bca599831f9f95f9cea110d9d8d67133bbd9bdf
Damon-Lee Pointon <51855558+DLBPointon@users.noreply.github.com>
2024-07-21 20:08:28 +0100

    Update and rename cloudflare-template.sh to prod.sh

--- prod.sh
+++ prod.sh
@@ -2,12 +2,12 @@
 ## change to "bin/sh" when necessary
 
 auth_email=""                                       # The email used to login 'https://dash.cloudflare.com'
-auth_method="token"                                 # Set to "global" for Global API Key or "token" for Scoped API Token
+auth_method="global"                                 # Set to "global" for Global API Key or "token" for Scoped API Token
 auth_key=""                                         # Your API Token or Global API Key
 zone_identifier=""                                  # Can be found in the "Overview" tab of your domain
-record_name=""                                      # Which record you want to be synced
+record_name="$1"                                      # Which record you want to be synced
 ttl=3600                                            # Set the DNS TTL (seconds)
-proxy="false"                                       # Set the proxy to true or false
+proxy="true"                                       # Set the proxy to true or false
 sitename=""                                         # Title of site "Example Site"
 slackchannel=""                                     # Slack Channel #example
 slackuri=""                                         # URI for Slack WebHook "https://hooks.slack.com/services/xxxxx"
